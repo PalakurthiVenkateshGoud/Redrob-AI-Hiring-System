0955d497eb277295a9e152d17bac55e69a4825f9
Palakurthi Venkatesh Goud <vgoud2005@gmail.com>
2026-06-09 12:45:57 +0530

    Remove debug files folder

--- backup_debug_files/ab.py
+++
@@ -1,9 +0,0 @@
-import json
-
-count = 0
-
-with open(r"[PUB] India_runs_data_and_ai_challenge\India_runs_data_and_ai_challenge\candidates.jsonl", "r", encoding="utf-8") as f:
-    for _ in f:
-        count += 1
-
-print("Candidates:", count)
